4e0bdbffddf9aaaa3b4fd1066b45b22e647c437e
Alfredo Garcia <alfredo.garcia@disoftec.mx>
2024-06-14 12:56:47 -0600

    whoami request

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  res.json({ipaddress: req.headers.host, language: req.headers['accept-language'], software: req.headers['user-agent']});
+  console.log(req.headers);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
